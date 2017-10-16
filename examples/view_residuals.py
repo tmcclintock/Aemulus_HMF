d1e8db6076484eb3b079d01dce139c9fa8dddcae
Tom McClintock <tmcclintock89@gmail.com>
2017-10-16 09:28:40 -0500

    Made a plot better.

--- examples/view_residuals.py
+++ examples/view_residuals.py
@@ -7,9 +7,13 @@ import matplotlib.pyplot as plt
 zs, lMs, nus, R, Re, box, snap = np.genfromtxt("R_T08.txt", unpack=True)
 zsf, lMsf, nusf, Rf, Ref, box, snap = np.genfromtxt("R_f.txt", unpack=True)
 
-plt.plot(nus, R, marker='.', ls='', markersize=1, c='b')
-plt.plot(nusf, Rf, marker='.', ls='', markersize=1, c='r')
+plt.plot(nus, R, marker='.', ls='', markersize=1, c='b', label=r"$N_{T08}$")
+plt.plot(nusf, Rf, marker='.', ls='', markersize=1, c='r', label=r"$N_{T08}(1+f)$")
 
 ylim = .1
 plt.ylim(-ylim, ylim)
+plt.legend(loc=0, frameon=False)
+plt.xlabel(r"$\nu$")
+plt.ylabel(r"$\%\ Diff$")
+plt.subplots_adjust(bottom=0.15, left=0.15)
 plt.show()
