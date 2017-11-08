7599c1efaf1b9cbb1257538db23936fd9d382937
Tom McClintock <tmcclintock89@gmail.com>
2017-11-08 10:55:44 -0700

    Cleaned up the aemHMF.py file and made it easier to use.

--- aemHMF/aemHMF.py
+++ aemHMF/aemHMF.py
@@ -7,76 +7,60 @@ import residual_gp
 from n_t08 import peak_height
 import numpy as np
 
-#A placeholder cosmo_dict
-cd = {"om":0.3,"ob":0.05,"ol":1.-0.3,"ok":0.0,"h":0.7,"s8":0.77,"ns":0.96,"w0":-1.0,"wa":0.0,"Neff":3.0}
+#This is the formal the cosmological dictionary should take.
+cd = {"om":0.3,"ob":0.05,"ol":1.-0.3,"ok":0.0,"h":0.7,"s8":0.77,"ns":0.96,"w0":-1.0,"Neff":3.0}# "wa":0.0 is assumed internally
 
 
 class Aemulus_HMF(object):
     
     def __init__(self):
-        self.n_t08 = None#n_t08.n_t08(cd)
+        self.n_t08 = None
         self.residualgp = residual_gp.residual_gp()
 
     def set_cosmology(self, cosmo_dict):
-        #This is a requirement for now
-        cosmo_dict["wa"] = 0.0
+        cosmo_dict["wa"] = 0.0 #Aemulus simulations don't have wa
         self.n_t08 = n_t08.n_t08()
         self.n_t08.set_cosmology(cosmo_dict)
 
     def dndlM(self, M, a):
         return self.n_t08.dndlM(M, a)
 
-    def n_bin(self, Mlow, Mhigh, a, with_f=False):
-        n_t08 = self.n_t08.n_bin(Mlow, Mhigh, a)
-        if not with_f: return n_t08
-        M = np.mean(Mlow, Migh)
-        nu = peak_height(M, a)
-        z = 1-1./a
-        f, fcov = self.f.predict_f(nu, np.ones_like(nu)*z)
-        return n_t08*(1+f)
+    def n_bin(self, Mlow, Mhigh, a):
+        return self.n_t08.n_bin(Mlow, Mhigh, a)
 
-    def n_bins(self, Mbins, a, with_f=False, with_scatter=False):
-        n_t08 = self.n_t08.n_bins(Mbins, a)
-        if not with_f: return n_t08
-        M = np.mean(Mbins, 1)
-        nu = np.array([peak_height(Mi, a) for Mi in M])
-        z = 1-1./a
-        if not with_scatter: f, cov = self.f.predict_f(nu, np.ones_like(nu)*z)
-        else: f = self.f.sample_f(nu, np.ones_like(nu)*z)
-        return n_t08*(1.+f)
+    def n_bins(self, Mbins, a):
+        return self.n_t08.n_bins(Mbins, a)
 
-    def residual_realization(self, M, a, N_samples):
+    def residual_realization(self, M, a, Nrealizations):
         z = 1-1./a
         nu = np.array([peak_height(Mi, a) for Mi in M])
-        return np.array([self.residualgp.residual_realization(nu, np.ones_like(nu)*z) for i in range(N_samples)])
+        return np.array([self.residualgp.residual_realization(nu, np.ones_like(nu)*z) for i in range(Nrealizations)])
 
 if __name__ == "__main__":
-    cd = {"om":0.3,"ob":0.05,"ol":1.-0.3,"ok":0.0,"h":0.7,"s8":0.77,"ns":0.96,"w0":-1.0,"wa":0.0,"Neff":3.0}
+    cosmo_dictionary = {"om":0.3,"ob":0.05,"ol":1.-0.3,"ok":0.0,"h":0.7,"s8":0.77,"ns":0.96,"w0":-1.0,"wa":0.0,"Neff":3.0}
     a = 1.0 #Scale factor
     hmf = Aemulus_HMF()
-    hmf.set_cosmology(cd)
-    M = np.logspace(11, 16, num=11, base=10)
-    Mbins = np.array([M[:-1], M[1:]]).T
-    Mave = np.mean(Mbins, 1)
+    hmf.set_cosmology(cosmo_dictionary)
+    Medges = np.logspace(11, 16, num=11)
+    Mbins = np.array([Medges[:-1], Medges[1:]]).T
+    M = np.mean(Mbins, 1)
     n = hmf.n_bins(Mbins, a)
-    nt08 = hmf.n_bins(Mbins, a, with_f=False)
     V = 1050.**3 #Mpc/h ^3
+    fs = hmf.residual_realization(M, a, 50)
 
     import matplotlib.pyplot as plt
     fig, axarr = plt.subplots(2, sharex=True)
-    axarr[0].loglog(Mave, n*V, c='k', label=r"$N_{\rm emu}$")
-    axarr[0].loglog(Mave, nt08*V, c='r', ls='--', label=r"$N_{\rm T08}$")
-    plt.sca(axarr[0])
-    plt.legend(loc=0, fontsize=14)
-    axarr[1].scatter(Mave, (n-nt08)/nt08, c='k')
-    fs = hmf.residual_realization(Mave, a, 50)
+    axarr[0].loglog(M, n*V, c='k', label=r"$N_{\rm emu}$")
+    axarr[0].legend(loc=0, fontsize=14, frameon=False)
+    
     for i in range(len(fs)):
-        ns = nt08*(1+fs[i])
-        axarr[1].plot(Mave, (ns-nt08)/nt08, c='blue', alpha=0.2, zorder=-1)
+        ns = n*(1+fs[i])
+        axarr[1].plot(M, (n-ns)/ns, c='blue', alpha=0.2, zorder=-1)
+        
     axarr[1].axhline(0, c='k', ls='-')
     axarr[1].set_xlabel(r"$M\ {\rm M_\odot}/h$")
-    axarr[0].set_ylabel(r"number")
-    axarr[1].set_ylabel(r"frac diff")
+    axarr[0].set_ylabel(r"number in bin")
+    axarr[1].set_ylabel(r"residual realizations")
     axarr[1].set_ylim(-0.05, 0.05)
     plt.subplots_adjust(hspace=0.0, left=0.15, bottom=0.15)
     plt.show()
