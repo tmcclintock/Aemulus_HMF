fd5ab46d1ea8599d2d92dc48cbcb91fe8fd053b8
Tom McClintock <tmcclintock89@gmail.com>
2017-12-02 19:07:05 -0700

    Working.

--- aemHMF/__init__.py
+++ aemHMF/__init__.py
@@ -9,67 +9,75 @@ import numpy as np
 class Aemulus_HMF(object):
 
     #This is the format the cosmological dictionary should take.
-    defaul_cosmology = {"om":0.3,"ob":0.05,"ol":1.-0.3,"ok":0.0,"h":0.7,"s8":0.77,"ns":0.96,"w0":-1.0,"Neff":3.0}# "wa":0.0 is assumed internally
+    default_cosmology = {"Omega_m":0.3,
+                         "Omega_b":0.05,
+                         "h":0.7,
+                         "sigma8":0.77,
+                         "n_s":0.96,
+                         "w0":-1.0,
+                         "N_eff":3.0,
+                         "wa":0.0} #This is a requirement
 
     def __init__(self):
         self.tinkerMF = None
         self.residualgp = residual_gp.residual_gp()
 
-    def set_cosmology(self, cosmo_dict):
-        cosmo_dict["wa"] = 0.0 #Aemulus simulations don't have wa
+    def set_cosmology(self, cosmo_dict=None):
+        if not cosmo_dict:
+            print "No cosmology set. Using default cosmology."
+            cosmo_dict = self.default_cosmology
+        if 'wa' in cosmo_dict:
+            if cosmo_dict['wa'] != 0:
+                raise Exception("AemulusError: 'wa' must be set to 0")
         self.tinkerMF = tinkerMF.tinkerMF()
+        cosmo_dict['wa'] = 0.0 #Aemulus simulations don't have wa
         self.tinkerMF.set_cosmology(cosmo_dict)
 
-    def set_default_cosmology(self):
-        self.tinkerMF = tinkerMF.tinkerMF()
-        self.tinkerMF.set_cosmology(self.defaul_cosmology)
-
-    def Mtosigma(self, M, a):
+    def Mtosigma(self, M, z):
         if type(M) is list or type(M) is np.ndarray:
-            return np.array([self.tinkerMF.Mtosigma(Mi, a) for Mi in M])
+            return np.array([self.tinkerMF.Mtosigma(Mi, z) for Mi in M])
         else:
-            return self.tinkerMF.Mtosigma(M, a)
+            return self.tinkerMF.Mtosigma(M, z)
 
-    def multiplicity(self, M, a):
+    def multiplicity(self, M, z):
         if type(M) is list or type(M) is np.ndarray:
-            return np.array([self.tinkerMF.GM(Mi, a) for Mi in M])
+            return np.array([self.tinkerMF.GM(Mi, z) for Mi in M])
         else:
-            return self.tinkerMF.GM(M, a)
+            return self.tinkerMF.GM(M, z)
 
-    def multiplicity_sigma(self, sigma, a):
+    def multiplicity_sigma(self, sigma, z):
         if type(sigma) is list or type(sigma) is np.ndarray:
-            return np.array([self.tinkerMF.Gsigma(sigmai, a) for sigmai in sigma])
+            return np.array([self.tinkerMF.Gsigma(sigmai, z) for sigmai in sigma])
         else:
-            return self.tinkerMF.Gsigma(sigma, a)
+            return self.tinkerMF.Gsigma(sigma, z)
 
-
-    def dndlM(self, M, a):
+    def dndlM(self, M, z):
         if type(M) is list or type(M) is np.ndarray:
-            return np.array([self.tinkerMF.dndlM(Mi, a) for Mi in M])
+            return np.array([self.tinkerMF.dndlM(Mi, z) for Mi in M])
         else:        
-            return self.tinkerMF.dndlM(M, a)
+            return self.tinkerMF.dndlM(M, z)
 
-    def n_bin(self, Mlow, Mhigh, a):
-        return self.tinkerMF.n_bin(Mlow, Mhigh, a)
+    def n_bin(self, Mlow, Mhigh, z):
+        return self.tinkerMF.n_bin(Mlow, Mhigh, z)
 
-    def n_bins(self, Mbins, a):
-        return self.tinkerMF.n_bins(Mbins, a)
+    def n_bins(self, Mbins, z):
+        return self.tinkerMF.n_bins(Mbins, z)
 
-    def residual_realization(self, M, a, Nrealizations=1):
-        z = 1-1./a
-        nu = np.array([self.tinkerMF.peak_height(Mi, a) for Mi in M])
+    def residual_realization(self, M, z, Nrealizations=1):
+        nu = np.array([self.tinkerMF.peak_height(Mi, z) for Mi in M])
         return np.array([self.residualgp.residual_realization(nu, np.ones_like(nu)*z) for i in range(Nrealizations)])
 
 if __name__ == "__main__":
-    a = 1.0 #Scale factor
     hmf = Aemulus_HMF()
-    hmf.set_default_cosmology()
+    hmf.set_cosmology()
     Medges = np.logspace(11, 16, num=11)
+    z = 0.02
     Mbins = np.array([Medges[:-1], Medges[1:]]).T
     M = np.mean(Mbins, 1)
-    n = hmf.n_bins(Mbins, a)
-    fs = hmf.residual_realization(M, a, 50)
-
+    n = hmf.n_bins(Mbins, z)
+    fs = hmf.residual_realization(M, z, 50)
+    print n
+    """
     import matplotlib.pyplot as plt
     fig, axarr = plt.subplots(2, sharex=True)
     axarr[0].loglog(M, n)
@@ -77,3 +85,4 @@ if __name__ == "__main__":
         ns = n*(1+fs[i])
         axarr[1].plot(M, (n-ns)/ns, alpha=0.2)
     plt.show()
+    """
--- aemHMF/tinkerMF.py
+++ aemHMF/tinkerMF.py
@@ -1,7 +1,6 @@
 """
 This file contains the actual Tinker08 mass function. It uses emu to get the mass function parameters.
 """
-import cosmocalc as cc
 from classy import Class
 from cluster_toolkit import bias
 import numpy as np
@@ -21,58 +20,53 @@ class tinkerMF(object):
     def __init__(self):
         self.t_08 = emu.emu()
 
-    def set_cosmology(self, cosmo_dict, usecc=False):
+    def set_cosmology(self, cosmo_dict):
         self.cosmo_dict = cosmo_dict
-        self.rhom = cosmo_dict['om']*rhocrit #Msun h^2/Mpc^3
-        if usecc:
-            cc.set_cosmology(cosmo_dict)
-        else:
-            params = {
-                'output': 'mPk', #linear only
-                'h': cosmo_dict['h'],
-                #'A_s': cosmo_dict['As'],
-                'sigma8': cosmo_dict['s8'],
-                'n_s': cosmo_dict['ns'],
-                'w0_fld': cosmo_dict['w0'],
-                'wa_fld': 0.0,
-                'Omega_b': cosmo_dict['ob'],
-                'Omega_cdm': cosmo_dict['om'] - cosmo_dict['ob'],
-                'Omega_Lambda': 1.- cosmo_dict['om'],
-                'N_eff':cosmo_dict['Neff'],
-                'P_k_max_1/Mpc':10.,
-                'z_max_pk':10.
-            }
-            self.classcosmo = Class()
-            self.classcosmo.set(params)
-            self.classcosmo.compute()
-            Nm = 100
-            Nz = 10
-            M = np.logspace(11, 17, num=Nm) #Msun/h
-            R = self.MtoR(M) #Mpc already
-            z = np.linspace(0, 3, num=Nz)
-            sigmas = np.zeros((Nz, Nm))
-            for i in range(Nz):
-                #sigmas[i] = np.array([self.classcosmo.sigma(Ri, z[i]) for Ri in R])
-                h = self.cosmo_dict['h']
-                k = np.logspace(-5, 1, num=1000) #h Mpc^-1
-                p = np.array([self.classcosmo.pk_lin(ki, z[i]) for ki in k])*h**3 #Mpc^3/h^3
-                sigmas[i] = np.sqrt(bias.sigma2_at_M(M, k/h, p, self.cosmo_dict['om']))
-            #self.sigma_spl = RBS(z, np.log(M), np.log(sigmas.T)) #If using interp2d
-            self.sigma_spl = RBS(z, np.log(M), np.log(sigmas)) #If using RBS for real
-        self.usecc = usecc
+        self.rhom = cosmo_dict['Omega_m']*rhocrit #Msun h^2/Mpc^3
+        params = {
+            'output': 'mPk', #linear only
+            'h': cosmo_dict['h'],
+            #'A_s': cosmo_dict['A_s'],
+            'sigma8': cosmo_dict['sigma8'],
+            'n_s': cosmo_dict['n_s'],
+            'w0_fld': cosmo_dict['w0'],
+            'wa_fld': 0.0,
+            'Omega_b': cosmo_dict['Omega_b'],
+            'Omega_cdm': cosmo_dict['Omega_m'] - cosmo_dict['Omega_b'],
+            'Omega_Lambda': 1.- cosmo_dict['Omega_m'],
+            'N_eff':cosmo_dict['N_eff'],
+            'P_k_max_1/Mpc':10.,
+            'z_max_pk':10.
+        }
+        self.classcosmo = Class()
+        self.classcosmo.set(params)
+        self.classcosmo.compute()
+        Nm = 1000
+        Nz = 20
+        M = np.logspace(11, 17, num=Nm) #Msun/h
+        R = self.MtoR(M) #Mpc already
+        z = np.linspace(0, 3, num=Nz)
+        sigmas = np.zeros((Nz, Nm))
+        for i in range(Nz):
+            h = self.cosmo_dict['h']
+            k = np.logspace(-5, 1, num=1000) #h Mpc^-1
+            p = np.array([self.classcosmo.pk_lin(ki, z[i]) for ki in k])*h**3 #Mpc^3/h^3
+            sigmas[i] = np.sqrt(bias.sigma2_at_M(M, k/h, p, self.cosmo_dict['Omega_m']))
+        #self.lnsigma_spl = RBS(z, np.log(M), np.log(sigmas.T)) #If using interp2d
+        self.lnsigma_spl = RBS(z, np.log(M), np.log(sigmas)) #If using RBS for real
         cos = self.cos_from_dict(cosmo_dict)
         self.t08_slopes_intercepts = self.t_08.predict_slopes_intercepts(cos)
         
     def cos_from_dict(self, cosmo_dict):
         cd = cosmo_dict
-        om = cd['om']
-        ob = cd['ob']
+        om = cd['Omega_m']
+        ob = cd['Omega_b']
         w0 = cd['w0']
-        ns = cd['ns']
+        ns = cd['n_s']
         h  = cd['h']
-        Neff = cd['Neff']
+        Neff = cd['N_eff']
         #As = cd['As']
-        s8 = cd['s8']
+        s8 = cd['sigma8']
         H0 = h*100
         Obh2 = ob*h*h
         Och2 = (om-ob)*h*h
@@ -90,13 +84,14 @@ class tinkerMF(object):
         self.B = 2.0/(ed * gnd2 * gamma_d2 + gnf2 * gamma_f2)
         return
         
-    def merge_t08_params(self, a):
-        k = a-0.5
+    def merge_t08_params(self, z):
+        k = 1./(1.+z)-0.5
         d0,d1,f0,f1,g0,g1 = self.t08_slopes_intercepts
         d = d0 + k*d1
         e = np.array([1.0]) #Default Tinker08 value
         f = f0 + k*f1
         g = g0 + k*g1
+        #a = 1./(1.+z)
         #print "in merge",a, d ,e, f, g
         self.t08_params = np.array([d, e, f, g]).flatten()
         return
@@ -104,80 +99,69 @@ class tinkerMF(object):
     def MtoR(self, M):
         return (M/(4./3.*np.pi*self.rhom))**(1./3.)/self.cosmo_dict['h'] #Lagrangian radius in Mpc
 
-    def Mtosigma(self, M, a):
-        if self.usecc:
-            return cc.sigmaMtophat(M, a)
-        else:
-            #return np.exp(self.sigma_spl(1./a-1, np.log(M))[0]) #If using interp2d
-            return np.exp(self.sigma_spl(1./a-1, np.log(M))[0][0]) #If using RBS for real
+    def sigmaM(self, M, z):
+        #return np.exp(self.sigma_spl(z, np.log(M))[0]) #If using interp2d
+        return np.exp(self.lnsigma_spl(z, np.log(M))[0][0]) #If using RBS for real
         
-    def Gsigma(self, sigma, a):
-        if not hasattr(self, "a"):
-            self.a = a
-            self.merge_t08_params(a)
+    def Gsigma(self, sigma, z):
+        if not hasattr(self, "redshift"):
+            self.redshift = z
+            self.merge_t08_params(z)
             self.calc_normalization() #Recalculate B
-        if self.a != a:
-            self.a = a
-            self.merge_t08_params(a)
+        if self.redshift != z:
+            self.redshift = z
+            self.merge_t08_params(z)
             self.calc_normalization() #Recalculate B
         d,e,f,g = self.t08_params
         return self.B*((sigma/e)**-d + sigma**-f) * np.exp(-g/sigma**2)
 
-    def GM(self, M, a):
-        if not hasattr(self, "a"):
-            self.a = a
-            self.merge_t08_params(a)
+    def GM(self, M, z):
+        if not hasattr(self, "redshift"):
+            self.redshift = z
+            self.merge_t08_params(z)
             self.calc_normalization() #Recalculate B
-        if self.a != a:
-            self.a = a
-            self.merge_t08_params(a)
+        if self.redshift != z:
+            self.redshift = z
+            self.merge_t08_params(z)
             self.calc_normalization() #Recalculate B
-        sigma = self.Mtosigma(M, a)
-        return self.Gsigma(sigma, a)
+        sigma = self.sigmaM(M, z)
+        return self.Gsigma(sigma, z)
 
-    def dndlM(self, M, a):
-        gsigma = self.GM(M, a)
-        dM = 1e-6*M
-        dlnsiginvdm = np.log(self.Mtosigma(M-dM/2,a)/self.Mtosigma(M+dM/2, a))/dM
+    def dndlM(self, M, z):
+        gsigma = self.GM(M, z)
+        dM = 1e-8*M
+        dlnsiginvdm = (self.lnsigma_spl(z, np.log(M-dM*0.5))[0,0] - self.lnsigma_spl(z, np.log(M+dM*0.5))[0][0])/dM
         return gsigma * self.rhom * dlnsiginvdm
 
-    def n_bin(self, Mlow, Mhigh, a):
-        M = np.logspace(np.log10(Mlow), np.log10(Mhigh), num=1000)
+    def n_bin(self, Mlow, Mhigh, z):
+        M = np.logspace(np.log10(Mlow), np.log10(Mhigh), num=2000)
         lM = np.log(M)
-        dndlM = np.array([self.dndlM(Mi, a) for Mi in M])
+        dndlM = np.array([self.dndlM(Mi, z) for Mi in M])
         spl = IUS(lM, dndlM)
         return spl.integral(np.log(Mlow), np.log(Mhigh))
 
-    def n_bins(self, Mbins, a):
-        return np.array([self.n_bin(Mbi[0], Mbi[1], a) for Mbi in Mbins])
+    def n_bins(self, Mbins, z):
+        return np.array([self.n_bin(Mbi[0], Mbi[1], z) for Mbi in Mbins])
 
-    def peak_height(self, M, a):
-        return 1.686/self.Mtosigma(M,a)
+    def peak_height(self, M, z):
+        return 1.686/self.sigmaM(M, z)
 
 if __name__ == "__main__":
-    cos = {"om":0.3,"ob":0.05,"ol":1.-0.3,"ok":0.0,"h":0.7,"s8":0.77,"ns":0.96,"w0":-1.0,"wa":0.0,"Neff":3.0}
+    cos = {"Omega_m":0.3,
+           "Omega_b":0.05,
+           "h":0.7,
+           "sigma8":0.8,
+           "n_s":0.96,
+           "w0":-1.0,
+           "N_eff":3.0146}
     n = tinkerMF()
-    M = np.logspace(12, 16, num=10)
-
-    #import matplotlib.pyplot as plt
-    for i in xrange(0,1):
-        a = 1./(1.+i)
-        c = (1-a)/2
-        n.set_cosmology(cos, usecc=True)
-        dndlMcc = np.array([n.dndlM(Mi, a) for Mi in M])
-        #print dndlMcc
-        n.set_cosmology(cos, usecc=False)
-        dndlMcl = np.array([n.dndlM(Mi, a) for Mi in M])
-        #print dndlMcl
-        dif = (dndlMcl-dndlMcc)/dndlMcl
-        #print dif
-        #plt.plot(M, dif, label="z=%d"%i)
-        #plt.loglog(M, dndlMcc, label="z=%d"%i)
-        #plt.loglog(M, dndlMcl, label="z=%d"%i, ls=':')
-        for Mi, d in zip(M, dif):
-            print Mi, d
-    #plt.legend(loc=0)
-    #plt.xscale('log')
-    #plt.xlabel("Mass")
-    #plt.ylabel("$(n_{class}-n_{cc})/n_{class})$")
-    #plt.show()
+    n.set_cosmology(cos)
+    M = np.logspace(12, 16, num=100)
+    import matplotlib.pyplot as plt
+    for i in xrange(0,4):
+        z = float(i)
+        dndlM = np.array([n.dndlM(Mi, z) for Mi in M])
+        plt.loglog(M, dndlM, label="z=%d"%i, ls='-')
+    plt.legend(loc=0)
+    plt.xlabel("Mass")
+    plt.xscale('log'); plt.show()
