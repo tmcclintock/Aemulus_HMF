068d9ad67d0a08ccf7d27310a30a5e6990efcf95
Tom McClintock <tmcclintock89@gmail.com>
2017-12-08 12:47:45 -0700

    Worked on the scatter example.

--- examples/scatter_example.py
+++ examples/scatter_example.py
@@ -8,7 +8,7 @@ plt.rc("font", size=18, family='serif')
 if __name__ == "__main__":
     Volume = 1050.**3 #Mpc^3/h^3
     box = 0
-    Ombh2, Omch2, w, ns, ln10As, H0, Neff, sig8 = np.genfromtxt(AD.path_to_test_box_cosmologies())[box]
+    Ombh2, Omch2, w, ns, ln10As, H0, Neff, sig8 = AD.get_test_box_cosmologies()[box]
     h = H0/100.
     Ob = Ombh2/h**2
     Oc = Omch2/h**2
@@ -27,24 +27,26 @@ if __name__ == "__main__":
         if snapshot in [0, 1, 3, 4, 5, 7, 8]: continue
         a = sfs[snapshot]
         z = zs[snapshot]
-        path = AD.path_to_test_box_data(box, snapshot)
-        data = np.genfromtxt(path)
+        #path = AD.path_to_test_box_data(box, snapshot)
+        #data = np.genfromtxt(path)
+        data = AD.get_test_box_binned_mass_function(box, snapshot)
         N = data[:, 2]
         inds = (N > 0)
         data = data[inds]
         lMlo, lMhi, N, Mtot  = data.T
         M_bins = 10**np.array([lMlo, lMhi]).T
         M = Mtot/N
-        covpath = AD.path_to_test_box_covariance(box, snapshot)
-        cov = np.loadtxt(covpath)
+        #covpath = AD.path_to_test_box_covariance(box, snapshot)
+        #cov = np.loadtxt(covpath)
+        cov = AD.get_test_box_binned_mass_function_covariance(box, snapshot)
         err = np.sqrt(np.diag(cov))[inds]
 
-        N_aem = hmf.n_bins(M_bins, a, with_f=False)*Volume
+        N_aem = hmf.n_bins(M_bins, a)*Volume
         pdiff = (N-N_aem)/N_aem
         pdiff_err = err/N_aem
 
         Mf = np.logspace(np.log10(min(M)), np.log10(max(M)), num=100)
-        fs = hmf.f_scatter(M, a, 30)
+        fs = hmf.residual_realization(M, a, 30)
         for i in range(len(fs)):
             Ns = N_aem*(1+fs[i])
             axarr[1].plot(M, (N-Ns)/N_aem, c=colors[snapshot], alpha=0.2)
