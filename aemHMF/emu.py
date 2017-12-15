161cf1e3b622cf0d3f544cbcd574d4dee67c1a77
Tom McClintock <tmcclintock89@gmail.com>
2017-12-15 15:41:31 -0700

    Switched all dictionaries over to using the default Aemulus parameters.

--- aemHMF/emu.py
+++ aemHMF/emu.py
@@ -6,27 +6,20 @@ import george
 import os, inspect
 import numpy as np
 import scipy.optimize as op
-import Aemulus_data as AD
+import aemulus_data as AD
 data_path = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))+"/data_files/"
-#data_path = "../../fit_mass_functions/output/dfg_rotated/"
-R_matrix_path = data_path+"R2.txt"#+"R_matrix.txt"
-means_path    = data_path+"r_defg_means.txt"#"rotated_dfg_means.txt"
-vars_path     = data_path+"r_defg_vars.txt"#"rotated_dfg_vars.txt"
-cosmos_path   = data_path+"cosmos.txt"
-cosmos = np.genfromtxt(cosmos_path)
-cosmos = np.delete(cosmos, 0, 1)  #boxnum
-cosmos = np.delete(cosmos, 4, 1)  #ln10As
-#cosmos = np.delete(cosmos, 7, 1)  #sigma8
-cosmos = cosmos[:36] # FOR NOW
+R_matrix_path = data_path+"R2.txt"
+means_path    = data_path+"r_defg_means.txt"
+vars_path     = data_path+"r_defg_vars.txt"
+cosmos = AD.get_building_box_cosmologies()
+cosmos = cosmos[0:36] #Temporary
+cosmos = np.delete(cosmos, -1, 1) #Delete sigma8
+
 N_cosmos = len(cosmos)
 N_params = len(cosmos[0])
 
 means  = np.genfromtxt(means_path)
 var   = np.genfromtxt(vars_path)
-print "Truth:"
-for m,v in zip(means[0], var[0]):
-    print m, np.sqrt(v)
-print cosmos[0]
     
 class emu(object):
 
@@ -63,15 +56,23 @@ class emu(object):
         self.gplist = gplist
         return
 
-    def predict_slopes_intercepts(self, cosmo):
+    def predict_rotated_params(self, cosmo):
         x = np.atleast_2d(cosmo)
         y = self.means.T
-        params = np.array([gp.predict(yi, x)[0] for yi,gp in zip(y, self.gplist)])
-        print "Actual:",params
+        return np.array([gp.predict(yi, x)[0] for yi, gp in zip(y, self.gplist)])
 
+    def predict_slopes_intercepts(self, cosmo):
+        params = self.predict_rotated_params(cosmo)
         return np.dot(self.R, params).flatten()
 
 if __name__ == "__main__":
     t = emu()
     cos = cosmos[0]
-    print t.predict_slopes_intercepts(cos)
+    print "ombh2 omch2 w0 ns ln10As H0 Neff\n", cos
+    truth = t.means[0]
+    err = np.sqrt(t.vars[0])
+    pred = t.predict_rotated_params(cos)
+    print "Truth Err Pred"
+    for i in range(len(truth)):
+        print "%.2e %.2e %.2e"%(truth[i], err[i], pred[i])
+
--- aemHMF/tinkerMF.py
+++ aemHMF/tinkerMF.py
@@ -22,18 +22,20 @@ class tinkerMF(object):
 
     def set_cosmology(self, cosmo_dict):
         self.cosmo_dict = cosmo_dict
-        self.rhom = cosmo_dict['Omega_m']*rhocrit #Msun h^2/Mpc^3
+        Omh2 = cosmo_dict["Obh2"]+cosmo_dict["Och2"]
+        h = cosmo_dict["H0"]/100.
+        Omega_m = Omh2/h**2
+        self.rhom = Omega_m*rhocrit #Msun h^2/Mpc^3
         params = {
             'output': 'mPk', #linear only
-            'h': cosmo_dict['h'],
-            #'A_s': cosmo_dict['A_s'],
-            'sigma8': cosmo_dict['sigma8'],
+            'H0': cosmo_dict['H0'],
+            'ln10^{10}A_s': cosmo_dict['ln10^{10}A_s'],
             'n_s': cosmo_dict['n_s'],
             'w0_fld': cosmo_dict['w0'],
             'wa_fld': 0.0,
-            'Omega_b': cosmo_dict['Omega_b'],
-            'Omega_cdm': cosmo_dict['Omega_m'] - cosmo_dict['Omega_b'],
-            'Omega_Lambda': 1.- cosmo_dict['Omega_m'],
+            'omega_b': cosmo_dict['Obh2'],
+            'omega_cdm': cosmo_dict['Och2'],
+            'Omega_Lambda': 1.-Omega_m,
             'N_eff':cosmo_dict['N_eff'],
             'P_k_max_1/Mpc':10.,
             'z_max_pk':10.
@@ -47,63 +49,44 @@ class tinkerMF(object):
         
     def cos_from_dict(self, cosmo_dict):
         cd = cosmo_dict
-        om = cd['Omega_m']
-        ob = cd['Omega_b']
+        Och2 = cd['Och2']
+        Obh2 = cd['Obh2']
         w0 = cd['w0']
         ns = cd['n_s']
-        h  = cd['h']
+        H0  = cd['H0']
         Neff = cd['N_eff']
-        #As = cd['As']
-        s8 = cd['sigma8']
-        H0 = h*100
-        Obh2 = ob*h*h
-        Och2 = (om-ob)*h*h
-        out = np.array([Obh2, Och2, w0, ns, H0, Neff, s8])
-        print out
+        l10As = cd['ln10^{10}A_s']
+        out = np.array([Obh2, Och2, w0, ns, l10As, H0, Neff])
         return out
-        #return np.array([Obh2, Och2, w0, ns, As, H0, Neff])
 
-    def GM(self, M, z):
-        h = self.cosmo_dict['h']
-        Omega_m = self.cosmo_dict['Omega_m']
-        k = self.k_array #Mpc^-1
-        p = np.array([self.classcosmo.pk_lin(ki, z) for ki in k])*h**3 #[Mpc/h]^3
+    def get_tinker_parameters(self):
         d0,d1,e0,e1,f0,f1,g0,g1 = self.t08_slopes_intercepts
         x = (1.+z)-0.5
         d = d0 + x*d1
         e = e0 + x*e1
         f = f0 + x*f1
         g = g0 + x*g1
-        d, e, f, g = np.array([d,e,f,g]).flatten()
+        return  np.array([d,e,f,g]).flatten()
+
+
+    def GM(self, M, z):
+        h = self.cosmo_dict['H0']/100.
+        Omega_m = (self.cosmo_dict['Obh2']+self.cosmo_dict['Och2'])/h**2
+        k = self.k_array #Mpc^-1
+        p = np.array([self.classcosmo.pk_lin(ki, z) for ki in k])*h**3 #[Mpc/h]^3
+        d, e, f, g = self.get_tinker_parameters()
         return massfunction.G_at_M(M, k/h, p, Omega_m, d, e, f, g)
 
     def Gsigma(self, sigma, z):
-        d0,d1,e0,e1,f0,f1,g0,g1 = self.t08_slopes_intercepts
-        #d0,d1,f0,f1,g0,g1 = self.t08_slopes_intercepts
-        x = (1.+z)-0.5
-        d = d0 + x*d1
-        e = e0 + x*e1
-        f = f0 + x*f1
-        g = g0 + x*g1
-        d, e, f, g = np.array([d,e,f,g]).flatten()
+        d, e, f, g = self.get_tinker_parameters()
         return massfunction.G_at_sigma(sigma, d, e, f, g)
         
     def dndlM(self, M, z):
-        h = self.cosmo_dict['h']
-        Omega_m = self.cosmo_dict['Omega_m']
+        h = self.cosmo_dict['H0']/100.
+        Omega_m = (self.cosmo_dict['Obh2']+self.cosmo_dict['Och2'])/h**2
         k = self.k_array #Mpc^-1
         p = np.array([self.classcosmo.pk_lin(ki, z) for ki in k])*h**3 #[Mpc/h]^3
-        x = 1./(1.+z)-0.5
-        d0,d1,e0,e1,f0,f1,g0,g1 = self.t08_slopes_intercepts
-        d = d0 + x*d1
-        e = e0 + x*e1
-        f = f0 + x*f1
-        g = g0 + x*g1
-        d, e, f, g = np.array([d,e,f,g]).flatten()
-        #d=1.97 #for testing
-        #e=1.0 #for testing
-        #f=0.51 #for testing
-        #g=1.228 #for testing
+        d, e, f, g = self.get_tinker_parameters()
         return massfunction.dndM_at_M(M, k/h, p, Omega_m, d, e, f, g)*M
 
     def n_bin(self, Mlow, Mhigh, z):
@@ -115,17 +98,17 @@ class tinkerMF(object):
         return np.array([self.n_bin(Mbi[0], Mbi[1], z) for Mbi in Mbins])
     
     def peak_height(self, M, z):
-        h = self.cosmo_dict['h']
-        Omega_m = self.cosmo_dict['Omega_m']
+        h = self.cosmo_dict['H0']/100.
+        Omega_m = (self.cosmo_dict['Obh2']+self.cosmo_dict['Och2'])/h**2
         k = self.k_array #Mpc^-1
         p = np.array([self.classcosmo.pk_lin(ki, z) for ki in k])*h**3 #[Mpc/h]^3
         return 1.686/np.sqrt(bias.sigma2_at_M(M, k, p, Omega_m))
 
 if __name__ == "__main__":
-    cos = {"Omega_m":0.3,
-           "Omega_b":0.05,
-           "h":0.7,
-           "sigma8":0.8,
+    cos = {"Och2":0.12,
+           "Obh2":0.023,
+           "H0":70.,
+           "ln10^{10}A_s":3.093,
            "n_s":0.96,
            "w0":-1.0,
            "N_eff":3.0146}
